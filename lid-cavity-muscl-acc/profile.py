3fdbcefa8d07244b7bc1506b17510157ce98a056
amntorifune <58096339+amntorifune@users.noreply.github.com>
2021-11-07 11:20:32 +0900

    Add files via upload

--- lid-cavity-muscl-acc/profile.py
+++ lid-cavity-muscl-acc/profile.py
@@ -24,10 +24,10 @@ def draw128(Re, ls = '-'):
     x = x * 2 - 1
     y = y * 2 - 1
 
-    plt.plot(x, v, c = cm.viridis(Re / 10000), ls = ls, linewidth = 0.75, label = '%d (128x128)'%Re)
+    plt.plot(x, v, c = cm.viridis(Re / 10000), ls = ls, linewidth = 0.75)
     plt.plot(u, y, c = cm.viridis(Re / 10000), ls = ls, linewidth = 0.75, label = '%d (128x128)'%Re)
 
-def draw256(Re, c = 'b', ls = '-'):
+def draw256(Re, ls = '-'):
     fname = 'UVP_Re%d.ns2dccg256.csv'%Re
     x = []
     y = []
@@ -48,16 +48,16 @@ def draw256(Re, c = 'b', ls = '-'):
     x = x * 2 - 1
     y = y * 2 - 1
 
-    plt.plot(x, v, c = cm.viridis(Re / 10000), ls = ls, linewidth = 0.75, label = '%d (256x256)'%Re)
+    plt.plot(x, v, c = cm.viridis(Re / 10000), ls = ls, linewidth = 0.75)
     plt.plot(u, y, c = cm.viridis(Re / 10000), ls = ls, linewidth = 0.75, label = '%d (256x256)'%Re)
 
 if __name__ == '__main__':
-    draw128(100)
-    draw128(400)
-    draw128(1000)
-    draw256(3200)
-    draw256(5000)
-    draw256(7500)
+    draw128(100  )
+    draw128(400  , ls = '--')
+    draw128(1000 )
+    draw256(3200 , ls = '--')
+    draw256(5000 )
+    draw256(7500 , ls = '--')
     draw256(10000)
     xt = np.linspace(- 1, 1, 11)
     yt = np.linspace(- 1, 1, 11)
